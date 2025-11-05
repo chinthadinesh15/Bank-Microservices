a39d8b8850b5ceaa9b2fe9444ca8ca7e9246be8b
Dinesh babu <chinthadineshbabu15@gmail.com>
2025-11-05 15:33:00 +0530

    loans microservices basic structure

--- Loans/src/main/java/in/dinesh/Loans/LoansApplication.java
+++ Loans/src/main/java/in/dinesh/Loans/LoansApplication.java
@@ -1,9 +1,36 @@
 package in.dinesh.Loans;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Loans microservice REST API Documentation",
+                description = "Bank Loans microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Dinesh Babu",
+                        email = "chinthadineshbabu15.com",
+                        url = "https://www.eazybytes.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.dinesh.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Loans microservice REST API Documentation",
+                url = "https://www.dinesh.com/swagger-ui.html"
+        )
+)
 public class LoansApplication {
 
 	public static void main(String[] args) {
