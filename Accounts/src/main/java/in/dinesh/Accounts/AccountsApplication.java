c68c24bd0f1fff52f3c0899b1d4417a34633ff94
Dinesh babu <chinthadineshbabu15@gmail.com>
2025-11-10 12:20:47 +0530

    profiles - Accounts

--- Accounts/src/main/java/in/dinesh/Accounts/AccountsApplication.java
+++ Accounts/src/main/java/in/dinesh/Accounts/AccountsApplication.java
@@ -1,15 +1,18 @@
 package in.dinesh.Accounts;
 
+import in.dinesh.Accounts.dto.AccountsContactInfoDto;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(AccountsContactInfoDto.class)
 @OpenAPIDefinition(
         info = @Info(
                 title = "Accounts API",
--- Accounts/src/main/java/in/dinesh/Accounts/controller/AccountsController.java
+++ Accounts/src/main/java/in/dinesh/Accounts/controller/AccountsController.java
@@ -1,6 +1,7 @@
 package in.dinesh.Accounts.controller;
 
 import in.dinesh.Accounts.constants.AccountsConstants;
+import in.dinesh.Accounts.dto.AccountsContactInfoDto;
 import in.dinesh.Accounts.dto.CustomerDto;
 import in.dinesh.Accounts.dto.ResponseDto;
 import in.dinesh.Accounts.service.IAccountsService;
@@ -10,7 +11,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -21,20 +24,28 @@ import org.springframework.web.bind.annotation.*;
         description = "CRUD REST API's to Create, update,fetch,delete")
 @RestController
 @RequestMapping(path="/api" , produces = MediaType.APPLICATION_JSON_VALUE)
-@AllArgsConstructor
 @Validated
 public class AccountsController {
 
-    private IAccountsService accountsService;
+    private final IAccountsService accountsService;
 
-    @Operation(
-            summary = "Create a new account",
-            description = "REST API's to create new Customer & Account"
-    )
-    @ApiResponse(
-            responseCode = "201",
-            description = "Account created successfully"
-    )
+    public AccountsController(IAccountsService accountsService) {
+        this.accountsService = accountsService;
+    }
+
+    @Value("${build.version}")
+
+    private String buildVersion;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private AccountsContactInfoDto accountsContactInfoDto;
+
+
+    @Operation(summary = "Create a new account", description = "REST API's to create new Customer & Account")
+    @ApiResponse(responseCode = "201", description = "Account created successfully")
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
         accountsService.createAccount(customerDto);
@@ -44,14 +55,8 @@ public class AccountsController {
                 .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
-    @Operation(
-            summary = "Fetch account details",
-            description = "REST API's to fetch account details"
-    )
-    @ApiResponse(
-            responseCode = "200",
-            description = "Account details fetched successfully"
-    )
+    @Operation(summary = "Fetch account details", description = "REST API's to fetch account details")
+    @ApiResponse(responseCode = "200", description = "Account details fetched successfully")
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam("mobileNumber")
                                                                @Pattern(regexp = "$|[0-9]{10}", message = "Mobile number must be 10 digits")
@@ -64,10 +69,7 @@ public class AccountsController {
 
     }
 
-    @Operation(
-            summary = "Update account details",
-            description = "REST API's to update account details"
-    )
+    @Operation(summary = "Update account details", description = "REST API's to update account details")
     @ApiResponses(
             value = {
                     @ApiResponse( responseCode = "200", description = "Account details updated successfully"),
@@ -88,10 +90,7 @@ public class AccountsController {
         }
     }
 
-    @Operation(
-            summary = "Delete account details",
-            description = "REST API's to delete account details"
-    )
+    @Operation(summary = "Delete account details", description = "REST API's to delete account details")
     @ApiResponses(
             value = {
                     @ApiResponse( responseCode = "200", description = "Account details deleted successfully"),
@@ -114,4 +113,50 @@ public class AccountsController {
                     .body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
         }
     }
+
+    @Operation(summary = "Fetch build info", description = "REST API's to fetch build info")
+    @ApiResponses(
+            value = {
+                    @ApiResponse( responseCode = "200", description = "Build info fetched successfully"),
+                    @ApiResponse( responseCode = "500", description = "An Error occurred. Please try again or contact Dev team.")
+            }
+    )
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(buildVersion);
+    }
+
+    @Operation(summary = "Fetch Java Version", description = "Get Java Version details that is installed into accounts microservices...")
+    @ApiResponses(
+            value = {
+                    @ApiResponse( responseCode = "200", description = "Build info fetched successfully"),
+                    @ApiResponse( responseCode = "500", description = "An Error occurred. Please try again or contact Dev team.")
+            }
+    )
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("JAVA_HOME"));
+    }
+
+
+    @Operation(summary = "Fetch Contact Info", description = "Get Contact Info details that is installed into accounts microservices...")
+    @ApiResponses(
+            value = {
+                    @ApiResponse( responseCode = "200", description = "Build info fetched successfully"),
+                    @ApiResponse( responseCode = "500", description = "An Error occurred. Please try again or contact Dev team.")
+            }
+    )
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountsContactInfoDto> getContactInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(accountsContactInfoDto);
+    }
+
+
+
 }
